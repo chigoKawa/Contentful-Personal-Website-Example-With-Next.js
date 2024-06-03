87b06fe86b7faa185000c693f5585ff10c2c8794
chigoriddim <106765797+chigoriddim@users.noreply.github.com>
2024-06-04 00:41:16 +0200

    troubleshooting weird vercel issue2

--- app/api/seed-space/route.ts
+++ app/api/seed-space/route.ts
@@ -1,104 +1,309 @@
 import { createClient } from "contentful-management";
 import { NextResponse } from "next/server";
 
-import 'any-observable/register/zen';
-import { Observable } from 'rxjs';
+const contentfulSpaceExportFile = require("@/json/contentful-space-export.json");
 
+async function cleanSpace(client: any) {
+  const entries = await client.entry.getMany({});
+  // delete entries
+  console.error(`Deleting!  entries`, entries);
+  for (const entry of entries.items) {
+    if (entry.sys.firstPublishedAt) {
+      console.log(`Unpublishing entry "${entry.sys.id}"`);
 
+      await client.entry.unpublish(
+        {
+          entryId: entry?.sys?.id,
+        },
+        { ...entry }
+      );
+    }
 
+    await client.entry.delete({
+      entryId: entry?.sys?.id,
+    });
+  }
 
+  // delete content types
 
-import { ctfImport } from "../../ctf-import.mjs";
-const contentfulSPaceExportFile = require("@/json/contentful-space-export.json");
+  const contentTypes = await client.contentType.getMany({});
+  let totalContentTypes = contentTypes.total;
+  console.log(`Deleting! ${totalContentTypes} content types`);
+  console.log(contentTypes);
+  for (const contentType of contentTypes.items) {
+    if (contentType.sys.firstPublishedAt) {
+      console.log(`Unpublishing content type "${contentType.sys.id}"`);
 
-export async function POST(request: Request) {
+      await client.contentType.unpublish({
+        contentTypeId: contentType.sys.id,
+      });
+    }
+
+    await client.contentType.delete({
+      contentTypeId: contentType.sys.id,
+    });
+  }
+}
+
+async function createAndPublishContentType(client: any, contentType: any) {
   try {
-    const res = await request.json();
-
-
-    const spaceId = res?.data?.space_id || "";
-    const managementToken = res?.data?.mgt_access_token || "";
-    const envId = res?.data?.envId || "master";
-
-    //  TODO clean up space if it has content
-    const scopedPlainClient = createClient(
+    const createdContentType = await client.contentType.createWithId(
+      { contentTypeId: contentType.sys.id },
       {
-        accessToken: managementToken,
-      },
+        name: contentType.name,
+        fields: contentType.fields,
+        displayField: contentType.displayField,
+      }
+    );
+
+    await client.contentType.publish(
+      { contentTypeId: createdContentType.sys.id },
       {
-        type: "plain",
-        defaults: {
-          spaceId: spaceId,
-          environmentId: envId,
+        sys: {
+          version: 1,
         },
       }
     );
 
-   
-
-    // const entries = await scopedPlainClient.entry.getMany({
-    //   query: {
-    //     skip: 10,
-    //     limit: 100,
-    //   },
-    // });
-
-    const importOptions = {
-      content: contentfulSPaceExportFile,
-      spaceId: spaceId,
-      managementToken: managementToken,
-      environmentId: envId,
-    };
-
-    // const importResp = await ctfImport(importOptions);
-
-    return NextResponse.json({ message: "importResp" });
+    console.log(`Content type ${contentType.sys.id} published.`);
   } catch (error) {
-    let errorMessage: string;
-    const anyError: any = error;
-    if (error instanceof Error) {
-      if (anyError?.errors && Array.isArray(anyError.errors)) {
-        errorMessage =
-          "Multiple errors occurred during Contentful import, check your credentials ";
-        const errorMessages = anyError.errors.map((err: any) => {
-          const originalError = err.error.originalError;
-          return originalError.message
-            ? originalError.message
-            : "An error occurred";
-        });
+    console.error(`Error creating content type ${contentType.sys.id}:`, error);
+    throw error;
+  }
+}
 
-        let messages = errorMessages;
+async function createEntriesWithoutPublishing(client: any, entries: any[]) {
+  const createdEntries = {};
 
-        try {
-          messages = JSON.parse(errorMessages);
-        } catch (error) {}
+  for (const entry of entries) {
+    try {
+      const createdEntry = await client.entry.createWithId(
+        { entryId: entry.sys.id, contentTypeId: entry.sys.contentType.sys.id },
+        { fields: entry.fields }
+      );
+      // @ts-ignore
+      createdEntries[entry.sys.id] = createdEntry;
+      console.log(`Entry created: ${createdEntry.sys.id}`);
+    } catch (error) {
+      console.error(`Error creating entry ${entry.sys.id}:`, error);
+      throw error;
+    }
+  }
 
-        return NextResponse.json({
-          messages: messages,
-          message: errorMessage,
-          hasError: true,
-        });
-      }
+  return createdEntries;
+}
 
-      if (error.name === "ContentfulMultiError") {
-        // Handle ContentfulMultiError specifically
+async function updateAndPublishEntries(
+  client: any,
+  entries: any[],
+  createdEntries: any
+) {
+  for (const entry of entries) {
+    try {
+      const entryToUpdate = createdEntries[entry.sys.id];
 
-        const originalError = anyError?.originalError;
-        if (originalError) {
-          errorMessage = originalError.message || "An unknown error occurred.";
-        } else {
-          errorMessage =
-            "Multiple errors occurred during Contentful import, check your credentials ";
+      // Update the entry if it has dependencies
+      if (entry.fields) {
+        for (const locale of Object.keys(entry.fields)) {
+          for (const field of Object.keys(entry.fields[locale])) {
+            const fieldValue = entry.fields[locale][field];
+
+            // Check if the field value is a link
+            if (
+              fieldValue &&
+              fieldValue.sys &&
+              fieldValue.sys.type === "Link"
+            ) {
+              const linkedEntryId = fieldValue.sys.id;
+              if (!createdEntries[linkedEntryId]) {
+                console.error(
+                  `Dependency ${linkedEntryId} for entry ${entry.sys.id} not found.`
+                );
+                // throw new Error(
+                //   `Dependency ${linkedEntryId} for entry ${entry.sys.id} not found.`
+                // );
+              }
+            }
+          }
         }
-
-        console.error("ContentfulMultiError details:", error);
-      } else {
-        errorMessage = error.message;
       }
-    } else {
-      errorMessage = String(error);
+
+      const updatedEntry = await client.entry.update(
+        { entryId: entryToUpdate.sys.id },
+        { sys: entryToUpdate.sys, fields: entryToUpdate.fields }
+      );
+
+      const publishedEntry = await client.entry.publish(
+        {
+          entryId: updatedEntry.sys.id,
+        },
+        {
+          ...updatedEntry,
+        }
+      );
+      console.log(`Entry published: ${publishedEntry.sys.id}`);
+    } catch (error) {
+      console.error(
+        `Error updating or publishing entry ${entry.sys.id}:`,
+        error
+      );
+      throw error;
+    }
+  }
+}
+
+async function createAndPublishAsset(client: any, asset: any) {
+  try {
+    const createdAsset = await client.asset.createWithId(
+      { assetId: asset.sys.id },
+      {
+        fields: {
+          ...asset.fields,
+          file: {
+            "en-US": {
+              contentType: asset.fields.file["en-US"].contentType,
+              fileName: asset.fields.file["en-US"].fileName,
+              upload: `https:${asset.fields.file["en-US"].url}`,
+            },
+          },
+        },
+      }
+    );
+
+    console.log(`Asset created: ${createdAsset.fields.file}`);
+
+    // await client.asset.processForAllLocales({ assetId: createdAsset.sys.id });
+    const processedAsset = await client.asset.processForAllLocales(
+      { assetId: createdAsset.sys.id },
+      { ...createdAsset }
+    );
+
+    // Wait for the asset to be ready
+    let assetReady = false;
+    while (!assetReady) {
+      const assetStatus = await client.asset.get({
+        assetId: processedAsset.sys.id,
+      });
+      if (assetStatus.fields.file["en-US"].url) {
+        assetReady = true;
+      } else {
+        console.log("Waiting for asset to be processed...");
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second before checking again
+      }
     }
 
+    // Wait for the asset to be processed
+    // while (true) {
+    //   const assetStatus = await client.asset.get({
+    //     assetId: createdAsset.sys.id,
+    //   });
+    //   if (assetStatus.fields.file["en-US"].url) break;
+    //   console.log("Waiting for asset to be processed...");
+    //   await new Promise((resolve) => setTimeout(resolve, 1000));
+    // }
+
+    // Publish the asset
+    const publishedAsset = await client.asset.publish(
+      {
+        assetId: processedAsset.sys.id,
+      },
+      { ...processedAsset }
+    );
+
+    // await client.asset.publish({ assetId: createdAsset.sys.id });
+    console.log("Asset published:", publishedAsset?.sys.id);
+  } catch (error) {
+    console.error(`Error creating asset ${asset.sys.id}:`, error);
+    throw error;
+  }
+}
+
+async function createAndPublishEntry(client: any, entry: any) {
+  try {
+    // const createdEntry = await client.entry.createWithId(
+    //   { entryId: entry.sys.id, contentTypeId: entry.sys.contentType.sys.id },
+    //   { fields: entry.fields }
+    // );
+
+    client.entry
+      .create(
+        { contentTypeId: entry?.sys?.contentType?.sys?.id },
+        {
+          fields: entry.fields,
+        }
+      )
+      .then((entryRecord: any) => {
+        client.entry.publish(
+          { entryId: entryRecord.sys.id },
+          {
+            ...entryRecord,
+          }
+        );
+      })
+      .catch((er: any) => {
+        throw er;
+      });
+
+    // console.log(`Entry published: ${createdEntry.sys.id}`);
+  } catch (error) {
+    console.error(`Error creating entry ${entry.sys.id}:`, error);
+    throw error;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const res = await request.json();
+    const spaceId = res?.data?.space_id || "";
+    const managementToken = res?.data?.mgt_access_token || "";
+    const envId = res?.data?.envId || "master";
+
+    const client = createClient(
+      { accessToken: managementToken },
+      {
+        type: "plain",
+        defaults: { spaceId, environmentId: envId },
+      }
+    );
+    //  clean the space
+
+    await cleanSpace(client);
+
+    // Create and publish content types
+    for (const contentType of contentfulSpaceExportFile.contentTypes) {
+      await createAndPublishContentType(client, contentType);
+    }
+
+    // Create and publish assets
+    for (const asset of contentfulSpaceExportFile.assets) {
+      await createAndPublishAsset(client, asset);
+    }
+
+    // Create and publish entries
+    // for (const entry of contentfulSpaceExportFile.entries) {
+    //   await createAndPublishEntry(client, entry);
+    // }
+
+    // Create entries without publishing
+    const createdEntries = await createEntriesWithoutPublishing(
+      client,
+      contentfulSpaceExportFile.entries
+    );
+
+    // Update and publish entries
+    await updateAndPublishEntries(
+      client,
+      contentfulSpaceExportFile.entries,
+      createdEntries
+    );
+
+    return NextResponse.json({ message: "Space setup was successful" });
+  } catch (error) {
+    let errorMessage = "An unknown error occurred.";
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    console.error("Error during Contentful import:", error);
     return NextResponse.json({ message: errorMessage, hasError: true });
   }
 }
